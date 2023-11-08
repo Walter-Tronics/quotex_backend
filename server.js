9467ba3d184fc21d558811dfad3fa17c5b1f7a5a
Walter Tronics <waltertronics02@gmail.com>
2023-11-08 19:56:14 +0100

    add basic code for app to running

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+
+
+//initialize express
+const app = express();
+
+//set the view engine
+app.set('view engine', 'ejs');
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Let express recognize public folder for media files and style sheet
+app.use(express.static("public"));
